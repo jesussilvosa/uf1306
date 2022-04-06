f04ed3dd861ff2bff7e90c199b578bccc2eb5210
jesussilvosa <95342278+jesussilvosa@users.noreply.github.com>
2022-04-06 09:39:14 +0200

    ok

--- assets/js/formularios1.js
+++ assets/js/formularios1.js
@@ -1,6 +1,6 @@
 /*
  * Gestión de eventos y formularios en JavaScript
- */
+*/
 
 /*
     Función anónima
@@ -33,7 +33,6 @@ window.onload = function() {
         // es decir, del elemento más específico hacia afuera
 
     enviar.addEventListener('click', validarFormulario, false);
-
 }
 
 
@@ -50,19 +49,32 @@ function validarFormulario() {
     // Declaración de variables
     var valido = true;
     var expRegNombre = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,50}$/;
+    var expRegEmail = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    var expRegTfno = /^[6-9]{1}[0-9]{8}$/;
 
     // Objetos document HTML
     var formulario = document.getElementById("form")
     var nombre = document.getElementById("name");
     var email = document.getElementById("email");
+    var mensaje = document.getElementById("mensaje");
+    var edad = document.getElementById("edad");
+    var tfno = document.getElementById("tfno");
 
     // Validar con JavaScript que el campo “nombre” no esté vacío
-
     if (nombre.value == "") {
         // Si está vacío se mostrará el mensaje “El campo ‘nombre’ es obligatorio”
         alert("El campo 'nombre' es obligatorio");
         // y se pondrá el foco en el campo “nombre”
         nombre.focus();
+        valido = false;
+    }
+
+    // Validar con JavaScript que el campo “email" no esté vacío
+    else if (email.value == "") {
+        // Si está vacío se mostrará el mensaje “El campo ‘email' es obligatorio”
+        alert("El campo 'email' es obligatorio");
+        // y se pondrá el foco en el campo "email“
+        email.focus();
         valido = false;
     }
 
@@ -74,6 +86,34 @@ function validarFormulario() {
         // y se pondrá el foco en el campo “nombre”
         nombre.focus();
         valido = false;
+    }
+
+    // Validar con JavaScript que el campo “email" cumpla el patrón declarado
+
+    else if (!expRegEmail.exec(email.value)) {
+        // Si no es válido mostrará el mensaje de error
+        alert("El correo electrónico no parece un email válido");
+        // y se pondrá el foco en el campo “nombre”
+        email.focus();
+        valido = false;
+    }
+
+    else if ( !expRegTfno.exec(tfno.value) ) {
+        alert("El número no parece válido. Sólo número, sin espacios en blanco ni otros caracteres");
+        tfno.focus();
+        valido= = false;
+    }
+
+    else if ( mensaje.length > 255 ) {
+        alert("El texto ha excedido el límtite máximo de 255 caracteres");
+        mensaje.focus();
+        valido = false;
+    }
+
+    else if ( edad.value < 18 || edad.value > 120 ) {
+        alert("La edad debe ser entre 18 y 120");
+        edad.focus();
+        valido.false;
     }
 
     // Si todos los campos son válidos
